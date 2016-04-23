96ff77c5a8be7b238ff7116ca2d89a0338b5d995
Alexey Plutalov <demiazz.py@gmail.com>
2016-04-23 17:20:45 +0300

    Don't use `.babelrc` in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel',
         query: {
+          babelrc: false,
           presets: ['react', 'es2015', 'stage-0'],
           plugins: ['transform-runtime'],
         },
