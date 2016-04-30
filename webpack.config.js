bded27b4ebc040ba7777c184aa79b7f94fcde388
Alexey Plutalov <demiazz.py@gmail.com>
2016-05-01 02:20:05 +0300

    Fully rewrite webpack config. Use webpack and webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,158 @@
 const path = require('path');
 const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
 const HtmlPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const StatsPlugin = require('stats-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 
-module.exports = {
-  entry: [
-    path.join(__dirname, 'src', 'index.js'),
-  ],
+// Environment
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+// Paths
+
+const src = path.join(__dirname, 'src');
+const dist = path.join(__dirname, 'dist');
+
+// Base configuration
+
+const config = {
+  entry: path.join(src, 'index.js'),
+
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: dist,
     publicPath: '/',
   },
+
   plugins: [
     new webpack.EnvironmentPlugin(['NODE_ENV']),
-    new webpack.optimize.OccurenceOrderPlugin(),
     new HtmlPlugin({
-      template: 'src/index.tpl.html',
-      inject: 'body',
+      template: path.join(src, 'index.ejs'),
+      inject: false,
       filename: 'index.html',
     }),
   ],
+
   module: {
-    loaders: {
-      js: {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          babelrc: false,
-          presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['transform-runtime'],
-        },
-      },
-      css: {
-        test: /\.css$/,
-        loader: ['style-loader', 'css-loader', 'postcss-loader'],
-      },
-      json: {
+    loaders: [
+      {
         test: /\.json$/,
         loader: 'json',
       },
-    },
+    ],
   },
-  postcss: [autoprefixer],
 };
+
+// Output configuration
+
+config.output.filename = isProduction ? '[name]-[hash].js' : '[name].js';
+
+// Development tools
+
+config.devtool = isProduction ? 'source-map' : 'eval';
+
+if (!isProduction) {
+  config.output.pathinfo = true;
+}
+
+// JavaScript loader
+
+const jsLoader = {
+  test: /\.js$/,
+  exclude: /node_modules/,
+  loader: 'babel',
+  query: {
+    babelrc: false,
+    presets: ['react', 'es2015', 'stage-0'],
+    plugins: ['transform-runtime'],
+  },
+};
+
+if (isProduction) {
+  jsLoader.query.plugins.push('transform-react-constant-elements');
+  jsLoader.query.plugins.push('transform-react-inline-elements');
+} else {
+  jsLoader.query.presets.push('react-hmre');
+}
+
+config.module.loaders.push(jsLoader);
+
+// CSS loader
+
+const cssLoader = {
+  test: /\.css$/,
+};
+const cssLoaders = ['css-loader', 'postcss-loader'];
+
+if (isProduction) {
+  cssLoader.loader = ExtractTextPlugin.extract('style-loader', cssLoaders);
+} else {
+  cssLoader.loaders = ['style-loader'].concat(cssLoaders);
+}
+
+config.module.loaders.push(cssLoader);
+
+// PostCSS
+
+const postcssPlugins = [autoprefixer];
+
+if (isProduction) {
+  postcssPlugins.push(cssnano);
+}
+
+config.postcss = postcssPlugins;
+
+// Plugins
+
+if (isProduction) {
+  config.plugins.push(
+    new ExtractTextPlugin('[name]-[hash].css')
+  );
+
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+        screw_ie8: true,
+      },
+    })
+  );
+
+  config.plugins.push(
+    new webpack.optimize.OccurenceOrderPlugin()
+  );
+
+  config.plugins.push(
+    new StatsPlugin('manifest.json', {
+      chunkModules: false,
+      source: false,
+      chunks: false,
+      modules: false,
+      assets: true,
+    })
+  );
+} else {
+  config.plugins.push(
+    new webpack.NoErrorsPlugin()
+  );
+
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin()
+  );
+}
+
+// Development server
+
+config.devServer = {
+  colors: true,
+  compress: false,
+  host: '0.0.0.0',
+  port: 3000,
+  inline: true,
+  historyApiFallback: true,
+};
+
+
+module.exports = config;
