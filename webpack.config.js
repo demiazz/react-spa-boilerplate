a6763c3bba60636a2f4e598c8fd55191b6c70a7f
Alexey Plutalov <demiazz.py@gmail.com>
2016-05-02 18:11:38 +0300

    Add comments for `webpack.config.js`

--- webpack.config.js
+++ webpack.config.js
@@ -41,34 +41,59 @@ const config = {
   },
 
   plugins: [
+    // NOTE: provide `process.env.NODE_ENV` to browser environment. Useful
+    //       for development and debug purposes.
     new webpack.EnvironmentPlugin(['NODE_ENV']),
+    // NOTE: generate index.html file from template. HtmlPlugin generate
+    //       script in head without `defer` option by default. You can
+    //       provide additional options which will be available in template.
+    //       For example, you can have config for error tracking service, and
+    //       provide them to templates.
+    //       See example in `src/index.ejs`.
+    //       For more information see documentation for HtmlPlugin.
     new HtmlPlugin({
       template: path.join(src, 'index.ejs'),
       inject: false,
       filename: 'index.html',
     }),
+    // NOTE: load fetch polyfill by Github as global `fetch`. Useful for old
+    //       browsers. Polyfill use native `fetch` if available.
     new webpack.ProvidePlugin({
       fetch: 'imports?this=>global!exports?global.fetch!whatwg-fetch',
     }),
+    // NOTE: define `__DEV__` constant. Some packages use them internally.
+    //       For example, react-router use them for showing warnings when
+    //       `__DEV__` is true.
     new webpack.DefinePlugin({
       __DEV__: !isProduction,
     }),
   ],
 
   resolve: {
+    // NOTE: resolve `.jss` files as JS modules. It's contains just JS code,
+    //       but used for providing JSS stylesheets. You can use `.js` for them
+    //       instead.
     extensions: ['', '.js', '.jss'],
   },
 };
 
 // Output configuration
 
+// NOTE: add hash prefix to filenames. Useful for cache reset between deploys.
 config.output.filename = isProduction ? '[name]-[hash].js' : '[name].js';
 
 // Development tools
 
+// NOTE: generate full source maps for production environment. For example, you
+//       can upload them to Sentry on deploy. But generation of source map
+//       insufficient.
+//       In development environment use eval code generation which have high
+//       build and rebuild speed.
 config.devtool = isProduction ? 'source-map' : 'eval';
 
 if (!isProduction) {
+  // NOTE: include comments with information about the modules. It's helpful for
+  //       debug reasons.
   config.output.pathinfo = true;
 }
 
@@ -148,27 +173,35 @@ config.postcss = function postcssPacks() {
 // Plugins
 
 if (isProduction) {
+  // NOTE: extract styles to different files. Reduce JS file size.
   config.plugins.push(
     new ExtractTextPlugin('[name]-[hash].css')
   );
 
-  // config.plugins.push(
-  //   new webpack.optimize.UglifyJsPlugin({
-  //     compressor: {
-  //       warnings: false,
-  //       screw_ie8: true,
-  //     },
-  //   })
-  // );
-
+  // NOTE: enable code minification with UglifyJS. Disable support IE8 and less.
   config.plugins.push(
-    new webpack.optimize.OccurenceOrderPlugin()
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+        screw_ie8: true,
+      },
+    })
   );
 
+  // NOTE: optimize occurence order modules and chunks. Entry have higher
+  //       priority.
+  config.plugins.push(
+    new webpack.optimize.OccurenceOrderPlugin(true)
+  );
+
+  // NOTE: enable gzip compression. Modern web-servers and browsers can load
+  //       gzipped files.
   config.plugins.push(
     new CompressionPlugin()
   );
 
+  // NOTE: generate manifest based on build statistics. May be used with
+  //       template engines (for example, if you use Rails, or Django).
   config.plugins.push(
     new StatsPlugin('manifest.json', {
       chunkModules: false,
@@ -186,6 +219,12 @@ if (isProduction) {
 
 // Development server
 
+// NOTE: webpack-dev-server config. Don't use compression. Listen `0.0.0.0`
+//       address (you can view application from mobile devices in you network
+//       or from browsers inside virtual machines).
+//       History API fallback enabled by default for supporting of react-router.
+//       `hot` and `inline` options not works, if setted in config. Sets them
+//       in `package.json` as command line arguments.
 config.devServer = {
   colors: true,
   compress: false,
